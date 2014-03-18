f0beed30e9dbd70e26884e0f7a6ad31872659707
Dmitry Kasimtsev <dmitry.kasimtsev@strikead.com>
2014-03-18 02:12:06 +0200

    save book content into file

--- mailserver/smtpd_custom.py
+++ mailserver/smtpd_custom.py
@@ -5,18 +5,11 @@ from email.parser import Parser
 class CustomSMTPServer(smtpd.SMTPServer):
     
     def process_message(self, peer, mailfrom, rcpttos, data):
-        # print data
-        res = Parser().parsestr(data)
-        print type(res)
-        print res.keys()
-        # print res.get_payload()[0]
-        # print "\n\n\n"
-        print res.get_payload()[1].get_payload(decode=True)
-        # print res
-        # print 'Receiving message from:', peer
-        # print 'Message addressed from:', mailfrom
-        # print 'Message addressed to  :', rcpttos
-        # print 'Message length        :', len(data)
+        message = Parser().parsestr(data)
+        book = message.get_payload()[1]
+        print book.keys()
+        f = open('/tmp/workfile.fb2', 'w') 
+        f.write(book.get_payload(decode=True))
         return
 
 server = CustomSMTPServer(('127.0.0.1', 1025), None)
